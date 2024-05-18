b107ade0aa3fc9e896fda57a532ea8287e0b2f1e
Giansimone Coccia <S1097845@studenti.univpm.it>
2024-05-18 19:03:31 +0200

    Modifica main

--- src/main.py
+++
@@ -1,9 +0,0 @@
-from face_detection import FaceDetection
-from segmentation import Segmentation
-
-
-fd = FaceDetection()
-fd.face_detection('C:\\Users\\gians_ji5genm\\OneDrive - Università Politecnica delle Marche\\UNIVPM\\Computer Visione e Deep Learning\\Progetto\\CelebAMask-HQ\\CelebAMask-HQ\\CelebA-HQ-img', 'C:\\Users\\gians_ji5genm\\OneDrive - Università Politecnica delle Marche\\UNIVPM\\Computer Visione e Deep Learning\\Progetto\\Risultati')
-
-sg= Segmentation()
-sg.process_images
